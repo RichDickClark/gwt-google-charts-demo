b6734095a1ed0bb71f067b6ff7b527d020133651
Clark Williams <rwilliams@ll.mit.edu>
2012-10-30 11:31:57 -0400

    add error bars to example

--- src/com/clark/demos/server/TableServiceImpl.java
+++ src/com/clark/demos/server/TableServiceImpl.java
@@ -15,14 +15,28 @@ public class TableServiceImpl extends RemoteServiceServlet implements
 		TableService {
 
 	@Override
-	public String getTable() {		
+	public String getTable() {				
 	    DataTable data = new DataTable();
+	    ColumnDescription curcol = null;
 	    data.addColumn( new ColumnDescription("Task", ValueType.TEXT, "Task") );
 	    data.addColumn( new ColumnDescription("Stemming", ValueType.NUMBER, "Stemming") );
+	    curcol = new ColumnDescription("Stemming_LB", ValueType.NUMBER, null);
+	    curcol.setCustomProperty("role", "interval");
+	    data.addColumn( curcol );
+	    curcol = new ColumnDescription("Stemming_UB", ValueType.NUMBER, null);
+	    curcol.setCustomProperty("role", "interval");
+	    data.addColumn( curcol );
 	    data.addColumn( new ColumnDescription("NoStemming", ValueType.NUMBER, "No Stemming") );
+	    curcol = new ColumnDescription("NoStemming_LB", ValueType.NUMBER, null);
+	    curcol.setCustomProperty("role", "interval");
+	    data.addColumn( curcol );
+	    curcol = new ColumnDescription("NoStemming_UB", ValueType.NUMBER, null);
+	    curcol.setCustomProperty("role", "interval");
+	    data.addColumn( curcol );
+	    
 	    try {
-			data.addRowFromValues( "Fire", 1.0, 0.8 );
-		    data.addRowFromValues( "Flood", 0.5, 0.65 );			
+			data.addRowFromValues( "Fire",  1.0, 0.85, 1.0, 0.8 , 0.75, 0.85 );
+		    data.addRowFromValues( "Flood", 0.5, 0.4,  0.6, 0.65, 0.60, 0.70 );			
 		} catch (TypeMismatchException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
